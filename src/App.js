ba087ab7860c4ef6d4159b4985a8fb991d579eb1
Pravallika <05b.pravallika@gmail.com>
2021-03-24 13:48:19 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
+  // let name = 'Sweety';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hellooo {name} </h1> */}
+      {/* <Header /> */}
+      {/* <Header title="This is a" /> (passing props) */}
+      <Header /> {/* <Header /> with default props */}
     </div>
   );
 }
